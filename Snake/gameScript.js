c91c7a2da6b35327bd0194c9b51018ad511704c1
Jatin Dhiman <dhimanjatin003@gmail.com>
2024-07-21 14:02:10 +0530

    Adding code

--- Snake/gameScript.js
+++ Snake/gameScript.js
@@ -1,6 +1,8 @@
 var lastPaintTime =0 ;
 const SNAKE_SPEED = 3;
-let inputDirection= { x:-1 ,y:0 }
+let inputDirection= { x:0 ,y:0 }
+let lastInputDirection = inputDirection;
+let food={x:15,y:10}
 const snakeBody = [
     {x:8,y:8},
     {x: 9, y: 8},
@@ -26,37 +28,52 @@ window.requestAnimationFrame(paint)
 
 function draw(){
      drawSnake();
+     drawFood();
 }
 function update(){
     gameBoard.innerHTML="";
     snakeMove(); 
+    snakeEatFood();                                                             
 }
+
 
 function drawSnake(){
      snakeBody.forEach((segment,index)=>{
         var snakeElement = document.createElement("div")
         snakeElement.style.gridColumnStart = segment.x;
         snakeElement.style.gridRowStart =segment.y;
-        snakeElement.innerHTML = index
+        // snakeElement.innerHTML = index
+        snakeElement.style.transform = "rotate(0deg)"; 
 
         if(index == 0){
             snakeElement.classList.add("head");
-            // if(inputDirection.x=1){
+             if(inputDirection.x == 1){
+                snakeElement.style.transform = "rotate(-90deg)";
+             }else if(inputDirection.x == -1){
+                snakeElement.style.transform = "rotate(90deg)";
 
-            // }else if(inputDirection.x=-1){
+             }else if(inputDirection.y == -1){
+                snakeElement.style.transform = "rotate(180deg)";
 
-            // }else if(inputDirection.y=-1){
+             }
+             else if(inputDirection.y == 1){
+                snakeElement.style.transform = "rotate(0deg)";
 
-            // }
-            // else if(inputDirection.y=1){
-
-            // }
+             }
         }else{
         snakeElement.classList.add("snake");
         }
         gameBoard.appendChild(snakeElement)
      })
 }
+function drawFood(){
+    var foodElement = document.createElement("div")
+        foodElement.style.gridColumnStart = food.x;
+        foodElement.style.gridRowStart =food.y;
+        foodElement.classList.add("food");
+        gameBoard.appendChild(foodElement);
+}
+
     
     function snakeMove(){
         inputDirection=getInputDirection();
@@ -73,17 +90,38 @@ function drawSnake(){
         window.addEventListener("keydown",e=>{
 
             switch(e.key){
-                case'ArrowUp' : inputDirection={x:0 , y:-1}
+                case'ArrowUp' : 
+                if(lastInputDirection.y ==1) break;
+                inputDirection={x:0 , y:-1}
                 break;
-                case'ArrowDown' : inputDirection={x:0 , y:1}
+                case'ArrowDown' :
+                if(lastInputDirection.y ==-1) break;
+                inputDirection={x:0 , y:1}
                 break;
-                case'ArrowLeft' : inputDirection={x:-1 , y:0}
+                case'ArrowLeft' :
+                if(lastInputDirection.x ==1) break; 
+                inputDirection={x:-1 , y:0}
                 break;
-                case'ArrowRight' : inputDirection={x:1 , y:0}
+                case'ArrowRight' :
+                if(lastInputDirection.x ==-1) break;
+                inputDirection={x:1 , y:0}
                 break;
                 default : inputDirection={x:0 , y:0}
             }
         }) 
+        lastInputDirection = inputDirection;
         return inputDirection;
 
+    }
+    function snakeEatFood(){
+        if(isEat()){
+            food=getFoodrandomPosition();
+        }
+    }
+
+    function isEat(){
+        return (snakeBody[0].x === food.x && snakeBody[0].y === food.y)
+    }
+    function getFoodrandomPosition(){
+        return
     }
