41ba4ada555bf5aa3e21f5f90a072249d8d7c768
Jatin Dhiman <dhimanjatin003@gmail.com>
2024-07-22 14:37:18 +0530

    Done

--- Snake/gameScript.js
+++ Snake/gameScript.js
@@ -1,17 +1,22 @@
 var lastPaintTime =0 ;
-const SNAKE_SPEED = 3;
+let SNAKE_SPEED = 3;
 let inputDirection= { x:0 ,y:0 }
 let lastInputDirection = inputDirection;
-let food={x:15,y:10}
+let gameOver=false;
+const EXPANSION_AMOUNT = 3;
+var score=0;
 const snakeBody = [
-    {x:8,y:8},
-    {x: 9, y: 8},
-    {x: 10, y: 8},
-    {x: 11, y: 8},
-    {x: 12, y: 8}
+    {x:8,y:8}
+    // {x: 9, y: 8},
+    // {x: 10, y: 8},
+    // {x: 11, y: 8},
+    // {x: 12, y: 8}
 ]
 
+let food=getFoodrandomPosition();
+
 const gameBoard = document.querySelector(".game-board");
+const scoreBox =document.getElementById("score")
 
 function paint(currentTime){
     var TimeSeconds =  (currentTime-lastPaintTime)/1000;
@@ -19,9 +24,10 @@ function paint(currentTime){
     if(TimeSeconds <1 /SNAKE_SPEED) return;
     lastPaintTime=currentTime;
     
-
+    if(gameOver != true){
     update();
     draw();
+    }
 }
 window.requestAnimationFrame(paint)
 
@@ -78,50 +84,109 @@ function drawFood(){
     function snakeMove(){
         inputDirection=getInputDirection();
 
-        for(i = snakeBody.length - 2;i>=0;i-- ){
+        for(let i = snakeBody.length - 2;i>=0;i-- ){
             snakeBody[i+1]={...snakeBody[i]}
         }
         snakeBody[0].x+=inputDirection.x;
         snakeBody[0].y+=inputDirection.y;
+        checkGameOver();
     }
 
-    function getInputDirection(){
-         
-        window.addEventListener("keydown",e=>{
-
-            switch(e.key){
-                case'ArrowUp' : 
-                if(lastInputDirection.y ==1) break;
-                inputDirection={x:0 , y:-1}
-                break;
-                case'ArrowDown' :
-                if(lastInputDirection.y ==-1) break;
-                inputDirection={x:0 , y:1}
-                break;
-                case'ArrowLeft' :
-                if(lastInputDirection.x ==1) break; 
-                inputDirection={x:-1 , y:0}
-                break;
-                case'ArrowRight' :
-                if(lastInputDirection.x ==-1) break;
-                inputDirection={x:1 , y:0}
-                break;
-                default : inputDirection={x:0 , y:0}
-            }
-        }) 
+    function getInputDirection() {
+        window.addEventListener("keydown", e => {
+     
+           switch (e.key) {
+              case 'ArrowUp':
+                 if (lastInputDirection.y == 1) break;
+                 inputDirection = {
+                    x: 0,
+                    y: -1
+                 }
+                 break;
+              case 'ArrowDown':
+                 if (lastInputDirection.y == -1) break;
+                 inputDirection = {
+                    x: 0,
+                    y: 1
+                 }
+                 break;
+              case 'ArrowLeft':
+                 if (lastInputDirection.x == 1) break;
+                 inputDirection = {
+                    x: -1,
+                    y: 0
+                 }
+                 break;
+              case 'ArrowRight':
+                 if (lastInputDirection.x == -1) break;
+                 inputDirection = {
+                    x: 1,
+                    y: 0
+                 }
+                 break;
+              default:
+                 inputDirection = {
+                    x: 0,
+                    y: 0
+                 }
+           }
+        })
         lastInputDirection = inputDirection;
         return inputDirection;
-
-    }
+     }
     function snakeEatFood(){
         if(isEat()){
+            score += 10;
+            scoreBox.innerHTML = score;
+            console.log("eated")
             food=getFoodrandomPosition();
+            SNAKE_SPEED++;
+            expendSnake();
         }
     }
 
     function isEat(){
-        return (snakeBody[0].x === food.x && snakeBody[0].y === food.y)
+        return snakeBody[0].x === food.x && snakeBody[0].y === food.y
     }
     function getFoodrandomPosition(){
-        return
+
+        let a,b, myCondition=true;
+        while(myCondition){
+            a=Math.ceil(Math.random()*16)
+           b=Math.ceil(Math.random()*16)
+            myCondition=snakeBody.some(segment=>{
+                return segment.x === a && segment.y === b
+            })
+        }
+
+        return{
+            x: a,
+            y:b
+        }
     }
+
+    function expendSnake(){
+        for(i=0 ; i<EXPANSION_AMOUNT; i++){
+            snakeBody.push(snakeBody[snakeBody.length-1]);
+        }
+    }
+     function checkGameOver(){
+        if(snakeOutofGrid()|| snakeIntersection()){
+              
+              alert("Game Over :  You Loose")
+              gameOver =true ;
+              location.reload();
+        }
+     }
+
+     function snakeOutofGrid(){
+        return snakeBody[0].x < 0 || snakeBody[0].x >16  ||snakeBody[0].y < 0 ||snakeBody[0].y>16
+     }
+     function snakeIntersection(){
+        for(let i=1; i<snakeBody.length;i++){
+            if(snakeBody[0].x===snakeBody[i].x && snakeBody[0].y === snakeBody[i].y){
+                return true
+            }
+        }
+        // return false
+     }
